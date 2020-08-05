7ac26acd27562dbff25ec23414c24c5d15f674a2
Bharat Poptwani <bharat.poptwani@gmail.com>
2020-08-05 19:00:51 +0530

    login form handled with appropriate states

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Card, Logo, Form, Input, Button } from '../components/AuthForm';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link, Redirect } from 'react-router-dom';
+import { Card, Logo, Form, Input, Button, Error } from '../components/AuthForm';
+import { useAuth } from '../context/auth';
 
 function Login() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const { setAuthTokens } = useAuth();
+
+  function postLogin() {
+    axios
+      .post('https://www.somePlace.com/auth/login', {
+        userName,
+        password,
+      })
+      .then((result) => {
+        if (result.status === 200) {
+          setAuthTokens(result.data);
+          setLoggedIn(true);
+        } else {
+          setIsError(true);
+        }
+      })
+      .catch((e) => {
+        setIsError(true);
+      });
+  }
+
+  if (isLoggedIn) {
+    return <Redirect to='/' />;
+  }
+
   return (
     <Card>
       <Logo src={'http://lorempixel.com/350/250/'} />
       <Form>
-        <Input type='email' placeholder='email' />
-        <Input type='password' placeholder='password' />
-        <Button>Sign In</Button>
+        <Input
+          type='username'
+          value={userName}
+          onChange={(e) => {
+            setUserName(e.target.value);
+          }}
+          placeholder='email'
+        />
+        <Input
+          type='password'
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          placeholder='password'
+        />
+        <Button onClick={postLogin}>Sign In</Button>
       </Form>
       <Link to='/signup'>Don't have an account?</Link>
+      {isError && (
+        <Error>The username or password provided were incorrect!</Error>
+      )}
     </Card>
   );
 }
